d330b51e43468c08ec2449ef18ec0b337a2e448b
Abhrajyoti00 <abhrajyoti00@gmail.com>
2021-01-09 12:38:00 +0530

    Initialized the Grid

--- 2048 Game/2048.py
+++ 2048 Game/2048.py
@@ -21,8 +21,55 @@ class Game2048(Frame):
         self.grid_cells = []
         self.init_grid()  #Initializes the Grid by addiing the grid cells
         self.init_matrix() #Initializes the Matrix by adding new 2's in the matrix after starting the game
-        self.update_grid_cells() #Changes backgroud and text color. (UI Changes)
+        self.update_grid_cells() #Changes background and text color. (UI Changes)
 
         self.mainloop()   # Actually runs the program
         
+
+
+    #INITIALIZING THE GRID
+
+    def init_grid(self):
+        
+        '''
+        Now we create another Frame within the frame, with the color and sizes given under the constants.py file
+        '''
+        
+        background = Frame(background, bg = c.BACKGROUND_COLOR_GAME, width = c.SIZE, height = c.SIZE)
+        
+        background.grid()   #Visualizing the background as a grid
+
+        #Now we need to add cells
+
+        for i in range(c.GRID_LEN):
+            grid_row = []   #Each row of Grid
+            for j in range(c.GRID_LEN):
+                #Here  the cell is created with sizes (400/4) = 100. Cell itself is a frame
+                cell = Frame(background, bg = c.BACKGROUND_COLOR_CELL_EMPTY, width = c.SIZE/c.GRID_LEN, height = c.SIZE/c.GRID_LEN) 
+                
+                cell.grid(row = i, column = j, padx = c.GRID_PADDING, pady = c.GRID_PADDING) # Adding a  cell at (i,j) with padding = 10
+                #Padding is the space between the cells
+                                 
+                t = Label(master=cell, text = '', bg = c.BACKGROUND_COLOR_CELL_EMPTY, 
+                justify = CENTER, font = c.FONT, width = 5, height = 2)  # Creating the label in the grid. By default the text is empty
+                t.grid()  
+                #Label covers  the cell completely. Just the label will get change. So we will append only the label.
+                grid_row.append(t) 
+
+            self.grid_cells.append(grid_row)  #Rows of Labels. In each grid cells there are labels. 
+
+    
+    #INITIALIZING THE MATRIX 
+
+    ''' Our UI Part is Completed. Now our task is to visualize them with the help of our matrix'''
+
+    def init_matrix(self):
+        self.matrix = Logics.start_game()   #Returns a 2d Matrix with all values 0. Using this we will reflect on UI
+        Logics.add_new_2(self.matrix)
+        Logics.add_new_2(self.matrix)
+         
+
+
+
+
 
